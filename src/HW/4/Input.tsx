8a4da808ae6a76258818ebde6bbf8c71cc7e1b73
dmitrijs <petja314@gmail.com>
2024-07-29 13:14:59 +0100

    initial commit

---
+++ src/HW/4/Input.tsx
@@ -0,0 +1,18 @@
+
+
+
+type InputPropsType = {
+	currentText: string // НУЖНО ПРОТИПИЗИРОВАТЬ
+	setCurrentText: (value : string) => void // НУЖНО ПРОТИПИЗИРОВАТЬ
+};
+
+export const Input = (props: InputPropsType) => {
+	const onChangeHandler = (event: any) => {
+		// НУЖНО ДОПИСАТЬ
+		props.setCurrentText(event.currentTarget.value)
+	};
+
+	return (
+	  <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
+	);
+};
